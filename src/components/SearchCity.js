54fba51554b0cd842599af8321cd7921d03bbcf9
Borzenko Mihail <mihail.borzenko@dev-pro.net>
2018-11-12 20:41:05 +0200

    Use Autocomplete component instead of native elements

--- src/components/SearchCity.js
+++ src/components/SearchCity.js
@@ -1,43 +1,19 @@
 import React, { Component } from 'react';
+import Autocomplete from 'react-google-autocomplete';
 
 class SearchCity extends Component {
     constructor(props){
         super(props);
-
-        this.state = {
-            enteredCity : null,
-        }
-
-        this.handleChangeInput = this.handleChangeInput.bind(this)
     }
-
-    componentDidMount(){
-        if (!window.google.maps.places) {
-            throw new Error(
-              '[Weather]: Google Maps Places library must be loaded. But something went wrong'
-            );
-        } else{
-            this.init();
-        }
-    };
-
-    init() {
-        const searchInput = this._searchInput;
-        this.autocompleteService = new window.google.maps.places.Autocomplete(searchInput);
-    }
-
-    handleChangeInput = (e) => {
-        this.setState({ enteredCity: e.target.value });
-    };
 
     render() {
         return(
             <form className="search-form">
-                <input id="searchInput"
-                   placeholder="Enter a city"
-                   onChange={ this.handleChangeInput }
-                   ref={ (node) => { this._searchInput = node }}/>
-                <button className="searchBtn">ADD</button>
+                <Autocomplete
+                    style={{width: '90%'}}
+                    onPlaceSelected={(place) => { this.props.onSubmit(place.name) }}
+                    types={['(regions)']}
+                />
             </form>
         )
     };
